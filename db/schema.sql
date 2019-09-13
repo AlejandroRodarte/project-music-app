350aed887ffc2dd5f1994d2bf915cccf1b71452d
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-09-13 17:47:15 -0600

    Insert new Band and Album sample data

--- db/schema.sql
+++ db/schema.sql
@@ -12,3 +12,14 @@ CREATE TABLE IF NOT EXISTS bands (
     created_at TIMESTAMP DEFAULT NOW(),
     updated_at TIMESTAMP DEFAULT NOW() ON UPDATE NOW()
 );
+
+CREATE TABLE IF NOT EXISTS albums (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    image_path VARCHAR(255) NOT NULL,
+    release_year INT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW() ON UPDATE NOW(),
+    band_id INT NOT NULL,
+    FOREIGN KEY(band_id) REFERENCES bands(id)
+);
