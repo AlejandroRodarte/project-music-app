e88fe7e957e847d07e1e5ba9bbcf3c647bff8068
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-09-13 13:04:24 -0600

    Testing Sorting on Bands Queries

--- src/main/java/com/rodarte/musicapp/models/controller/BandController.java
+++ src/main/java/com/rodarte/musicapp/models/controller/BandController.java
@@ -5,6 +5,7 @@ import com.rodarte.musicapp.models.service.BandService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,8 +19,27 @@ public class BandController {
     private BandService bandService;
 
     @GetMapping
-    public Page<Band> getAllBands(@RequestParam Integer page, @RequestParam Integer size) {
-        return bandService.getAllBands(PageRequest.of(page, size));
+    public Page<Band> getAllBands(
+            @RequestParam Integer page,
+            @RequestParam Integer size,
+            @RequestParam String sort
+    ) {
+
+        String[] sortArr = sort.split(":");
+
+        String sortParam = sortArr[0];
+        String sortDirection = sortArr[1];
+
+        Sort.Direction direction;
+
+        if (sortDirection.equals("asc")) {
+            direction = Sort.Direction.ASC;
+        } else {
+            direction = Sort.Direction.DESC;
+        }
+
+        return bandService.getAllBands(PageRequest.of(page, size, Sort.by(direction, sortParam)));
+
     }
 
 }
