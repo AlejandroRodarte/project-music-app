92d7f4ed67e0042b506a05a6fdd16dc62d2cc0ac
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-09-16 17:12:18 -0600

    SQL: Create view for Songs

---
+++ db/songs-complete-view.sql
@@ -0,0 +1,26 @@
+USE music_db;
+
+CREATE OR REPLACE view songs_complete_view AS
+	SELECT 
+		songs.id AS id,
+		songs.name AS name,
+		songs.track_number as track_number,
+		songs.duration as duration,
+		songs.created_at as created_at,
+		songs.updated_at as updated_at,
+		albums.id as album_id,
+		albums.name as album_name,
+		albums.image_path as album_image_path,
+		bands.id as band_id,
+		bands.name as band_name,
+		bands.image_path as band_image_path
+		FROM 
+			songs 
+		LEFT JOIN 
+			albums 
+		ON 
+			songs.album_id = albums.id 
+		LEFT JOIN 
+			bands 
+		ON 
+			albums.band_id = bands.id;
